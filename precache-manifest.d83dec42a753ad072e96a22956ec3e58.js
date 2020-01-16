081b58fcb3c8ac4fc6e703061214fe38329b9ae1
Pedro Pimenta <pedro.pimenta@bwtech.com>
2020-01-16 09:43:43 -0300

    1

---
+++ precache-manifest.d83dec42a753ad072e96a22956ec3e58.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc3028c58553df8025454f7121ed250c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "18e96d3b6c57f10d180a",
+    "url": "/static/js/2.d307dd63.chunk.js"
+  },
+  {
+    "revision": "fe25d0507bbacd5e67cd3e74c9e8f791",
+    "url": "/static/js/2.d307dd63.chunk.js.LICENSE"
+  },
+  {
+    "revision": "82dc21d1b53ff4b704bf",
+    "url": "/static/js/main.327d56ce.chunk.js"
+  },
+  {
+    "revision": "6d45957333f8e6c7f7c8",
+    "url": "/static/js/runtime-main.a23ecc42.js"
+  }
+]);
